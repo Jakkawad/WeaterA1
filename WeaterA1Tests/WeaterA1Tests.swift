1e0ecc2653a3ceb04720382cd6eecc9fe643dd51
Jakkawad <admin@all2sale-imac.local>
2016-09-23 15:48:58 +0700

    Initial Commit

---
+++ WeaterA1Tests/WeaterA1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  WeaterA1Tests.swift
+//  WeaterA1Tests
+//
+//  Created by admin on 9/23/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import XCTest
+@testable import WeaterA1
+
+class WeaterA1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
